39b0487ef7a6ebcc950e24b447dc42ec8423fc71
nat <nat.echlin@gmail.com>
2021-12-27 21:43:40 +0000

    completed general site structure, began jigging script

---
+++ auto jigger/jigger_script.js
@@ -0,0 +1,8 @@
+const addressJig = () => {
+            
+    const address = document.querySelector("#add_line1")
+    const amount = document.querySelector("#amount")
+    
+    // let consenants = []
+
+}
