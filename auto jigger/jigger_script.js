821e48f1d5289a2229c2cc5ab32d321a5fe9d2c6
nat <nat.echlin@gmail.com>
2021-12-28 20:13:40 +0000

    working address line 1

--- auto jigger/jigger_script.js
+++ auto jigger/jigger_script.js
@@ -11,22 +11,42 @@ const findConsenants = (address) => {
     return myArr
 }
 
-const getRandomInt = max => {
-    return Math.floor(Math.random() * max);
+// min inclusive, max exclusive
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 Array.prototype.insert = function(index,item) {
     this.splice(index,0,item);
 }
 
-const addressJig = () => {           
+const findLastNumIndex = charList => {
+    const NUMS = "0123456789".split("")
+    let i = 0
+    while (NUMS.includes(charList[i])) {
+        i++
+    }
+    return i
+}
+
+const sendOutput = jigs => {
+    jigsStr = jigs.join("\n")
+    const textArea = document.querySelector("#output")
+    textArea.style.display = "inline-flex"
+    textArea.value = jigsStr
+}
+
+const addressJig1 = (e) => {           
+    e.preventDefault()
+
     // init vars
     let jigs = []
     const address = document.querySelector("#add_line1").value
     const amount = parseInt(document.querySelector("#amount").value)
+    const lastNumIndex = findLastNumIndex(address.split(""))
     
     // locating consenants
-    let consenants = findConsenants(address)
+    const consenants = findConsenants(address)
 
     // random jigging phase
     for (let i = 0; i < amount; i++) {
@@ -34,21 +54,32 @@ const addressJig = () => {
         const charList = address.split("")
 
         // doubling 2 random consenants
-        const randomMax = getRandomInt(3)
+        const randomMax = getRandomInt(1,3)
         for (let i = 0; i < randomMax; i++) {
-            const ranPos = consenants[getRandomInt(consenants.length)]
+            const ranPos = consenants[getRandomInt(0, consenants.length - 1)]
             charList.insert(ranPos + 1, address[ranPos].toLowerCase())
         }
 
-        // adding a fullstop at a random pos
-        // todo
+        // adding 2 fullstops at any place after the street number
+        for (let i = 0; i <= 2; i++) {
+            const ranPos = getRandomInt(lastNumIndex + 1, address.length)
+            charList.insert(ranPos, ".")
+        }
 
+        // adding XXX jig
+        letters = "qwertyuiopasdfghjklzxcvbnm".toUpperCase().split("")
+        charList.insert(0, " ")
+        for (let i = 0; i < 3; i++) {
+            charList.insert(0, letters[getRandomInt(0,27)])
+        }
+
+        // adding this jig to list of all completed jigs
         jiggedAddress = charList.join("")
         jigs.push(jiggedAddress)
     }
     
-    const textbox = document.querySelector("#output")
-    textbox.style.display = "block"
-    textbox.setAttribute("rows", amount)
-    textbox.value = textbox.value + jigs.join("\n")
+    sendOutput(jigs)
+    return false
 }
+
+// addressJig()
--- auto jigger/testing/js.js
+++ auto jigger/testing/js.js
@@ -2,6 +2,21 @@
 //     "foo bar".length
 // )
 
-for (let i = 0; i < 5; i++) {
-    console.log(i)
-}
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+// x = 5
+// console.log(x-=1)
+
+// const findNums = charList => {
+//     const NUMS = "0123456789".split("")
+
+//     let i = 0
+//     while (NUMS.includes(charList[i])) {
+//         i++
+//     }
+//     return i
+// }
+
+
